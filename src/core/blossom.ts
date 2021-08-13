3588a00cbe34a7df1f5a1acffe6d4d960ea8dab4
Eric Rovell <ericrovell@gmail.com>
2021-08-13 14:45:15 +0300

    upd: core package, new plugins

--- src/core/blossom.ts
+++ src/core/blossom.ts
@@ -1,8 +1,16 @@
 import { extend } from "@ericrovell/blossom";
 import { pluginA11Y } from "@ericrovell/blossom/plugins/a11y";
+import { pluginXYZ } from "@ericrovell/blossom/plugins/xyz";
+import { pluginLAB } from "@ericrovell/blossom/plugins/lab";
+import { pluginLCH } from "@ericrovell/blossom/plugins/lch";
+import { pluginHWB } from "@ericrovell/blossom/plugins/hwb";
 
 extend([
-	pluginA11Y
+	pluginA11Y,
+	pluginXYZ,
+	pluginLAB,
+	pluginLCH,
+	pluginHWB
 ]);
 
 export * from "@ericrovell/blossom";
